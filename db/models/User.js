6c75c32649fd2f15195b43f888928892508db9a9
Ningyi <ningyi6@gmail.com>
2018-08-11 01:14:19 -0700

    add regular expression validation to validate user email and avatar url

--- db/models/User.js
+++ db/models/User.js
@@ -1,7 +1,32 @@
 const Sequelize = require('sequelize');
+const bcrypt = require('bcrypt-nodejs');
 const db = require('../db.js');
 const hash = require('../utilities/helps.js');
-const bcrypt = require('bcrypt-nodejs');
+
+const PASSWORD_MIN_LENGTH = 8;
+const PASSWORD_MAX_LENGTH = 30;
+
+const validationMessages = {
+  firstName: {
+    required: 'First name is required.',
+  },
+  lastName: {
+    required: 'Last name is required.',
+  },
+  userName: {
+    required: 'User name is required.',
+  },
+  email: {
+    required: 'Email address is required.',
+    email: 'Email address is not valid.',
+    unique: 'Email address already exists.',
+  },
+  password: {
+    required: 'Password is required.',
+    minLength: `Password is too short (min ${PASSWORD_MIN_LENGTH} characters).`,
+    maxLength: `Password is too long (max ${PASSWORD_MAX_LENGTH} characters).`,
+  },
+},
 
 const User = db.define('user', {
   userName: {
