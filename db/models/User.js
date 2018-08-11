6519d394695302c7e5cd75d03f3af24e1c993ac8
Ningyi <ningyi6@gmail.com>
2018-08-10 23:36:23 -0700

    add signup method

--- db/models/User.js
+++ db/models/User.js
@@ -83,7 +83,7 @@ let createUser = (user, callback) => {
       if (callback !== undefined) {
         callback({
           messageCode: 101, 
-          message: 'User email already exists'
+          message: 'User email already exists!'
         });
       }      
     } else {
@@ -108,13 +108,69 @@ let createUser = (user, callback) => {
   });
 }
 
+let signIn = (user, callback) => {
+  User
+  .sync({ force: false })
+  .then(() => {
+    // Retrieve objects from the database:
+    return User.findOne({ 
+      where: {
+        email: user.email
+      }
+    });
+  })
+  .then((existingUser) => {
+    console.log(existingUser);
+    if (existingUser !== null) {
+      if (existingUser.password === user.password) {
+        console.log('User login successfully!');
+        if (callback !== undefined) {
+          callback({
+            messageCode: 200, 
+            message: 'User login successfully!',
+            user: existingUser
+          });
+        } 
+      } else {
+        console.log('Wrong password');
+        if (callback !== undefined) {
+          callback({
+            messageCode: 103, 
+            message: 'Wrong password!'
+          });
+        }
+      }
+    } else {
+      console.log('User does not exist');
+      if (callback !== undefined) {       
+        callback({
+          messageCode: 104, 
+          message: 'User does not exist!'
+        });
+      }
+    }
+    db.close();      
+  })
+  .catch((err) => {
+    // Handle any error in the chain
+    console.error(err);
+    db.close();
+  });
+};
+
 createUser({
-  userName: 'MT2',
+  userName: 'MT',
   firstName: 'Miller',
   lastName: 'Tian',
   email: 'miller2@gamil.com',
   password: 'Welcome@123'
 });
 
+signIn({
+  email: 'miller@gamil.com',
+  password: 'Welcome@123' 
+});
+
 module.exports.createUser = createUser;
+module.exports.signIn = signIn;
 
