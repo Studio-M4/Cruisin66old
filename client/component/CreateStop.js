c0d3db079fff35061e98d1476ac1ee53108e9bdd
Henry Han <potatoeater0127@gmail.com>
2018-08-13 16:48:41 -0700

    (feat) Add CreateStop component.

--- client/component/CreateStop.js
+++ client/component/CreateStop.js
@@ -1,69 +1,105 @@
-import React from 'react';
-import { StyleSheet, Text, View, Image, TextInput, TouchableHighlight,ScrollView } from 'react-native';
-import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import React from "react";
+import { StyleSheet, Text, View, Image, TextInput, TouchableHighlight, ScrollView, Button } from "react-native";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 
 export default class CreateStop extends React.Component {
-    static navigationOptions = {
-        title: 'CreateStop',
-
-    };
+  static navigationOptions = {
+    title: "CreateStop"
+  };
   constructor(props) {
     super(props);
+    this.state = {photos: []}
+  }
+
+  _handleButtonPress() {
+    CameraRoll.getPhotos({
+      first: 20,
+      assetType: "Photos"
+    })
+      .then(r => {
+        this.setState({ photos: r.edges });
+      })
+      .catch(err => {
+        //Error Loading Images
+      });
   }
 
   render() {
     return (
       <ScrollView>
         <View style={styles.container}>
-            <Text style={styles.title}> Stop: </Text>
-            <GooglePlacesAutocomplete
-              placeholder='Search'
-              minLength={2} // minimum length of text to search
-              autoFocus={false}
-              returnKeyType={'search'} // Can be left out for default return key https://facebook.github.io/react-native/docs/textinput.html#returnkeytype
-              listViewDisplayed='auto'    // true/false/undefined
-              fetchDetails={true}
-              renderDescription={row => row.description} // custom description render
-              onPress={(data, details = null) => { // 'details' is provided when fetchDetails = true
-                console.log(data, details);
-              }}
-              
-              getDefaultValue={() => ''}
-              
-              query={{
-                // available options: https://developers.google.com/places/web-service/autocomplete
-                key: 'AIzaSyBHNIOJemEkEyO4gI_hask8BO6bJno9Q58',
-                language: 'en', // language of the results
-                types: 'geocode' // default: 'geocode'
-              }}
-              
-              styles={{
-                textInputContainer: {
-                  width: '100%'
-                },
-                description: {
-                  fontWeight: 'bold'
-                },
-                predefinedPlacesDescription: {
-                  color: '#1faadb'
-                }
-              }}
-              
-              nearbyPlacesAPI='GooglePlacesSearch' // Which API to use: GoogleReverseGeocoding or GooglePlacesSearch
-              GoogleReverseGeocodingQuery={{
+          <Text style={styles.title}> Stop: </Text>
+          <GooglePlacesAutocomplete
+            placeholder="Search"
+            minLength={2} // minimum length of text to search
+            autoFocus={false}
+            returnKeyType={"search"} // Can be left out for default return key https://facebook.github.io/react-native/docs/textinput.html#returnkeytype
+            listViewDisplayed="auto" // true/false/undefined
+            fetchDetails={true}
+            renderDescription={row => row.description} // custom description render
+            onPress={(data, details = null) => {
+              // 'details' is provided when fetchDetails = true
+              console.log(data, details);
+            }}
+            getDefaultValue={() => ""}
+            query={{
+              // available options: https://developers.google.com/places/web-service/autocomplete
+              key: "AIzaSyBHNIOJemEkEyO4gI_hask8BO6bJno9Q58",
+              language: "en", // language of the results
+              types: "" // default: 'geocode'
+            }}
+            styles={{
+              textInputContainer: {
+                width: "100%"
+              },
+              description: {
+                fontWeight: "bold"
+              },
+              predefinedPlacesDescription: {
+                color: "#1faadb"
+              }
+            }}
+            nearbyPlacesAPI="GooglePlacesSearch" // Which API to use: GoogleReverseGeocoding or GooglePlacesSearch
+            GoogleReverseGeocodingQuery={
+              {
                 // available options for GoogleReverseGeocoding API : https://developers.google.com/maps/documentation/geocoding/intro
-              }}
-              GooglePlacesSearchQuery={{
-                // available options for GooglePlacesSearch API : https://developers.google.com/places/web-service/search
-                rankby: 'distance',
-                types: 'food'
-              }}
+              }
+            }
+            GooglePlacesSearchQuery={{
+              // available options for GooglePlacesSearch API : https://developers.google.com/places/web-service/search
+              rankby: "distance",
+              types: "food"
+            }}
+            filterReverseGeocodingByTypes={[
+              "locality",
+              "administrative_area_level_3"
+            ]} // filter the reverse geocoding results by types - ['locality', 'administrative_area_level_3'] if you want to display only cities
+            debounce={1000} // debounce the requests in ms. Set to 0 to remove debounce. By default 0ms.
+          />
+          <Text style={styles.title}> Description: </Text>
+          <TextInput
+            editable={true}
+            multiline={true}
+            numberOfLines={6}
+            maxLength={100}
+            placeholder={"What so special here?"}
+          />
 
-              filterReverseGeocodingByTypes={['locality', 'administrative_area_level_3']} // filter the reverse geocoding results by types - ['locality', 'administrative_area_level_3'] if you want to display only cities
-
-              debounce={1000} // debounce the requests in ms. Set to 0 to remove debounce. By default 0ms.
-            />
-            <Text style={styles.title}> Description: </Text>
+          <Button title="Load Images" onPress={this._handleButtonPress} />
+          <ScrollView>
+            {this.state.photos.map((p, i) => {
+              return (
+                <Image
+                  key={i}
+                  style={{
+                    width: 300,
+                    height: 100
+                  }}
+                  source={{ uri: p.node.image.uri }}
+                />
+              );
+            })}
+          </ScrollView>
         </View>
       </ScrollView>
     );
@@ -72,24 +108,23 @@ export default class CreateStop extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#ccc',
-    alignItems: 'center',
-    width: '100%',
-    borderColor: '#000',
-    height: '100%'
+    alignItems: "center",
+    width: "100%",
+    borderColor: "#000",
+    height: "100%"
   },
- 
+
   title: {
     fontSize: 22,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     color: "#000",
     marginTop: 60
   },
   button: {
-    alignItems: 'center',
-    backgroundColor: '#336699',
+    alignItems: "center",
+    backgroundColor: "#336699",
     marginTop: 20,
     padding: 10,
-    width: 300,
-  },
+    width: 300
+  }
 });
