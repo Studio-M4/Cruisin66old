b231f2166c8f1dac303402e2ad4c5455887a5619
Henry Han <potatoeater0127@gmail.com>
2018-08-15 12:48:02 -0700

    (feat) Insert record into ItineraryStops when creating a new stop. Make eslint shut up.

--- server/controllers/stop.js
+++ server/controllers/stop.js
@@ -1,50 +1,49 @@
 // This is the file that pass request to db Stop model
 const db = require('../../db/models/');
 
-let getAllStops = (query, callback) => {
-  db.Stop.findAll({ 
+const getAllStops = (query, callback) => {
+  db.Stop.findAll({
     where: {
-      itineraryId: query.itineraryId
-    }
-  }) 
-  .then((stops) => {
-    console.log(stops);    
-    callback(null, stops);
+      itineraryId: query.itineraryId,
+    },
   })
-  .catch((err) => {
-    // Handle any error in the chain
-    console.error(err);
-    callback(err, null);
-  });
-}
+    .then((stops) => {
+      console.log(stops);
+      callback(null, stops);
+    })
+    .catch((err) => {
+      // Handle any error in the chain
+      console.error(err);
+      callback(err, null);
+    });
+};
 
-let getStopById = (query, callback) => {
+const getStopById = (query, callback) => {
   db.Stop.findOne({
     where: {
-      id: query.id
-    }
+      id: query.id,
+    },
   })
-  .then((stop) => {
-    console.log(stop);
-    callback(null, stop);
-  })
-  .catch((err) => {
-    console.error(err);
-    callback(err, null);
-  })
-}
+    .then((stop) => {
+      console.log(stop);
+      callback(null, stop);
+    })
+    .catch((err) => {
+      console.error(err);
+      callback(err, null);
+    });
+};
 
-let createStop = (newStop, callback) => {
+const createStop = (newStop, itineraryId, callback) => {
   db.Stop.create(newStop)
-  .then((createdStop) => {
-    console.log(createdStop);
-    callback(null, createdStop);
-  })
-  .catch((err) => {
-    console.error(err);
-    callback(err, null);
-  })
-}
+    .then(createdStop => createdStop
+      .setItineraries([itineraryId])
+      .then(() => callback(null, createdStop)))
+    .catch((err) => {
+      console.error(err);
+      callback(err, null);
+    });
+};
 
 module.exports.createStop = createStop;
 module.exports.getAllStops = getAllStops;
--- server/routes/createStop.js
+++ server/routes/createStop.js
@@ -1,15 +1,17 @@
-const db = require('../controllers/stop.js');
-const app = require('../app.js');
 const stop = require('express').Router();
+const db = require('../controllers/stop.js');
+
 
 stop.post('/stop', (req, res) => {
-  let newStop = {
+  const { itineraryId } = req.body;
+  const newStop = {
     name: req.body.name || '',
     description: req.body.description || '',
-    userId: req.body.userId || 8
+    address: req.body.address || '',
+    userId: req.body.userId || 8,
   };
 
-  db.createStop(newStop, (err, data) => {
+  db.createStop(newStop, itineraryId, (err, data) => {
     if (err) {
       res.sendStatus(500);
       res.send('Server side error happened');
